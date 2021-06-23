07b4c40d318e6ff927a107585d9ad732a9c39a3f
Riekr <gparmiggiani@gmail.com>
2021-06-23 15:09:40 +0200

    Compact FilteredTextSource (keep track of ranges)

--- src/org/riekr/jloga/io/FilteredTextSource.java
+++ src/org/riekr/jloga/io/FilteredTextSource.java
@@ -1,25 +1,54 @@
 package org.riekr.jloga.io;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
 
 public class FilteredTextSource implements TextSource {
 
+	private static class Range {
+		final int from;
+		int to;
+
+		private Range(int from) {
+			this.from = from;
+			this.to = from;
+		}
+
+		public Integer getSrcLine(int base, int line) {
+			int offset = line - base;
+			int res = from + offset;
+			return res > to ? null : res;
+		}
+	}
+
 	private final TextSource _tie;
-	private final Map<Integer, Integer> _lines = new HashMap<>();
+	private final TreeMap<Integer, Range> _lines = new TreeMap<>();
+	private int _size = 0;
 
 	public FilteredTextSource(TextSource tie) {
 		_tie = tie;
 	}
 
 	public void addLine(int line) {
-		_lines.put(_lines.size(), line);
+		Map.Entry<Integer, Range> entry = _lines.floorEntry(line);
+		if (entry == null) {
+			_lines.put(_size++, new Range(line));
+			return;
+		}
+		Range range = entry.getValue();
+		Integer tieLine = range.getSrcLine(entry.getKey(), line - 1);
+		if (tieLine == null) {
+			_lines.put(_size++, new Range(line));
+			return;
+		}
+		range.to++;
+		_size++;
 	}
 
 	@Override
 	public String getText(int line) throws ExecutionException, InterruptedException {
-		Integer tieLine = _lines.get(line);
+		Integer tieLine = getSrcLine(line);
 		if (tieLine != null)
 			return _tie.getText(tieLine);
 		return "";
@@ -27,10 +56,13 @@ public class FilteredTextSource implements TextSource {
 
 	@Override
 	public int getLineCount() {
-		return _lines.size();
+		return _size;
 	}
 
 	public Integer getSrcLine(int line) {
-		return _lines.get(line);
+		Map.Entry<Integer, Range> entry = _lines.floorEntry(line);
+		if (entry == null)
+			return null;
+		return entry.getValue().getSrcLine(entry.getKey(), line);
 	}
 }
