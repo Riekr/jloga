5ad36e78a677cdfbe83d710f190ee5497e4ef811
Riekr <gparmiggiani@gmail.com>
2021-06-30 15:01:49 +0200

    react

--- src/org/riekr/jloga/react/Observer.java
+++ src/org/riekr/jloga/react/Observer.java
@@ -1,8 +1,51 @@
 package org.riekr.jloga.react;
 
+import java.awt.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
 public interface Observer<T> {
+
+	static <T> Observer<T> uniq(Observer<T> observer) {
+		return new Observer<>() {
+			private T _lastValue;
+
+			@Override
+			public void onNext(T item) {
+				if (_lastValue != item) {
+					observer.onNext(item);
+					_lastValue = item;
+				}
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				observer.onError(t);
+			}
+		};
+	}
+
+	static <T> Observer<T> aync(Observer<T> observer) {
+		return new Observer<>() {
+			private final AtomicBoolean _enqueued = new AtomicBoolean();
+			private T _value;
+
+			@Override
+			public void onNext(T item) {
+				_value = item;
+				if (_enqueued.compareAndSet(false, true))
+					EventQueue.invokeLater(() -> {
+						if (_enqueued.compareAndSet(true, false))
+							observer.onNext(_value);
+					});
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				observer.onError(t);
+			}
+		};
+	}
 
 	static <T> Observer<T> from(Consumer<T> onNext, Consumer<Throwable> onError) {
 		return new Observer<>() {
@@ -23,4 +66,5 @@ public interface Observer<T> {
 	default void onError(Throwable t) {
 		t.printStackTrace(System.err);
 	}
+
 }
