29891f10624d67939ada590757d46e8173921d1f
Riekr <gparmiggiani@gmail.com>
2021-07-06 08:38:40 +0200

    About box

--- src/main/java/org/riekr/jloga/help/AboutPane.java
+++ src/main/java/org/riekr/jloga/help/AboutPane.java
@@ -1,13 +1,30 @@
 package org.riekr.jloga.help;
 
+import org.riekr.jloga.Main;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class AboutPane extends JOptionPane {
+
+	private static final String _HOMEPAGE = "https://github.com/Riekr/jloga";
+
+	private static String getVersion() {
+		String ver = Main.class.getPackage().getImplementationVersion();
+		if (ver != null)
+			return "v" + ver;
+		return "<i>dev " + DateTimeFormatter.ofPattern("dd/MM/uuuu").format(LocalDate.now()) + "</i>";
+	}
 
 	public AboutPane() {
 		super(
 				"<html>" +
-						"Welcome to <b>JLogA</b> v0.0.1<br><br>" +
+						"Welcome to <b>JLogA</b> " + getVersion() + "<br><br>" +
 						"<p>This program is free as in speech, without any warranty<br>" +
 						"even if i tried not to hurt your hardware.</p><br>" +
 						"<p>You can find source code in the below link; if you like<br>" +
@@ -18,6 +35,32 @@ public class AboutPane extends JOptionPane {
 				INFORMATION_MESSAGE,
 				DEFAULT_OPTION
 		);
-		add(new JLabel("<html><a href=\"\">https://github.com/Riekr/jloga</a></html>"), 1);
+		JLabel link = new JLabel("<html><a href=\"\">" + _HOMEPAGE + "</a></html>");
+		link.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+				if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+					try {
+						desktop.browse(URI.create(_HOMEPAGE));
+					} catch (Exception ex) {
+						ex.printStackTrace(System.err);
+					}
+				}
+			}
+		});
+		link.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				link.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				link.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+		});
+		add(link, 1);
 	}
+
 }
