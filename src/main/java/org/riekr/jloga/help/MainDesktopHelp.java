6078206f5e720f52910f7454b3f76deff267fce0
Riekr <gparmiggiani@gmail.com>
2021-07-16 10:05:27 +0200

    Fix for help arrows

--- src/main/java/org/riekr/jloga/help/MainDesktopHelp.java
+++ src/main/java/org/riekr/jloga/help/MainDesktopHelp.java
@@ -2,7 +2,7 @@ package org.riekr.jloga.help;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
 
 import static org.riekr.jloga.ui.UIUtils.getComponentHorizontalCenter;
 
@@ -10,14 +10,24 @@ public class MainDesktopHelp extends JComponent {
 
 	private static final char[] _ARROW = new char[]{'\u25B2'};
 
-	private final JComponent[] _components;
+	private final JComponent[] _leftComponents;
+	private final JComponent[] _rightComponents;
 
 	public MainDesktopHelp(JToolBar toolBar) {
-		_components = IntStream.range(0, toolBar.getComponentCount())
-				.mapToObj(toolBar::getComponentAtIndex)
-				.map((c) -> c instanceof JComponent ? (JComponent) c : null)
-				.filter((c) -> c != null && c.getToolTipText() != null)
-				.toArray(JComponent[]::new);
+		ArrayList<JComponent> lc = new ArrayList<>();
+		ArrayList<JComponent> rc = new ArrayList<>();
+		ArrayList<JComponent> arr = lc;
+		for (int i = 0; i < toolBar.getComponentCount(); i++) {
+			Component comp = toolBar.getComponentAtIndex(i);
+			if (comp instanceof Box.Filler) {
+				arr = rc;
+				continue;
+			}
+			if (comp instanceof JComponent && ((JComponent) comp).getToolTipText() != null)
+				arr.add((JComponent) comp);
+		}
+		_leftComponents = lc.toArray(new JComponent[0]);
+		_rightComponents = rc.toArray(new JComponent[0]);
 	}
 
 	@Override
@@ -25,22 +35,17 @@ public class MainDesktopHelp extends JComponent {
 		if (g instanceof Graphics2D)
 			((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.GRAY);
-		for (JComponent component : _components) {
-			drawCallout(g,
-					getComponentHorizontalCenter(component),
-					component.getToolTipText()
-			);
-		}
-	}
-
-	private void drawCallout(Graphics g, int x, String text) {
+		// impl
 		int my = 4;
 		int dy = 14;
-		char[] chars = text.toCharArray();
 		FontMetrics f = g.getFontMetrics();
 		int width = getWidth();
-		if (x <= (width / 2)) {
-			int leftMargin = getComponentHorizontalCenter(_components[_components.length - 3]) + 56;
+
+		int leftMargin = getComponentHorizontalCenter(_leftComponents[_leftComponents.length - 1]) + 32;
+		for (JComponent component : _leftComponents) {
+			int x = getComponentHorizontalCenter(component);
+			String text = component.getToolTipText();
+			char[] chars = text.toCharArray();
 			int radius = leftMargin - x;
 			int diameter = radius * 2;
 			g.drawArc(x, -radius + dy + my, diameter, diameter, 270, -90);
@@ -52,8 +57,13 @@ public class MainDesktopHelp extends JComponent {
 					x - (f.charWidth('\u25B2') / 2) - 1,
 					g.getFont().getSize() - 4 + my
 			);
-		} else {
-			int margin = 56;
+		}
+
+		for (JComponent component : _rightComponents) {
+			int x = getComponentHorizontalCenter(component);
+			String text = component.getToolTipText();
+			char[] chars = text.toCharArray();
+			int margin = 32;
 			int radius = width - margin - x;
 			int diameter = radius * 2;
 			g.drawArc(x - diameter, -radius + dy + my, diameter, diameter, -90, 90);
@@ -67,4 +77,5 @@ public class MainDesktopHelp extends JComponent {
 			);
 		}
 	}
+
 }
