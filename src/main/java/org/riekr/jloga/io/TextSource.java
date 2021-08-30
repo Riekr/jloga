279a94f2e537c214fa313f9a3dd6faf6e29277b9
Riekr <gparmiggiani@gmail.com>
2021-08-30 11:02:06 +0200

    Allowing 2 threads for indexing to better use fs cache

--- src/main/java/org/riekr/jloga/io/TextSource.java
+++ src/main/java/org/riekr/jloga/io/TextSource.java
@@ -18,7 +18,7 @@ import java.util.function.IntConsumer;
 
 public interface TextSource {
 
-	ExecutorService EXECUTOR = Executors.newSingleThreadExecutor();
+	ExecutorService EXECUTOR = new ThreadPoolExecutor(0, 2, 10L, TimeUnit.SECONDS, new SynchronousQueue<>());
 	AtomicReference<Future<?>> TEXT_FUTURE = new AtomicReference<>();
 
 	private static void executeRequestText(Runnable task) {
