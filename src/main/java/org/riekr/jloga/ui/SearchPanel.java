cc65552c953f6ab4885b8829aca54a5208441f50
Riekr <gparmiggiani@gmail.com>
2022-03-05 18:13:26 +0100

    Tabs weird behaviour

--- src/main/java/org/riekr/jloga/ui/SearchPanel.java
+++ src/main/java/org/riekr/jloga/ui/SearchPanel.java
@@ -7,7 +7,6 @@ import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -27,10 +26,12 @@ public class SearchPanel extends JComponent implements FileDropListener {
 	private final JSplitPane      _splitPane;
 	private final JTabbedPane     _bottomTabs;
 	private final TabNavigation   _bottomTabsNavigation;
+	private final JobProgressBar  _progressBar;
 
-	private final JobProgressBar _progressBar;
-	private       String         _title;
-	private       int            _searchId = 0;
+	private final int _level;
+
+	private String _title;
+	private int    _searchId = 0;
 
 	public SearchPanel(String title, String description, TextSource src, JobProgressBar progressBar, @Nullable TabNavigation tabNavigation) {
 		this(progressBar, 0, tabNavigation);
@@ -42,36 +43,28 @@ public class SearchPanel extends JComponent implements FileDropListener {
 
 
 	public SearchPanel(JobProgressBar progressBar, int level, @Nullable TabNavigation tabNavigation) {
+		_progressBar = progressBar;
+		_level = level;
+
 		setLayout(new BorderLayout());
 		_textArea = new VirtualTextArea(tabNavigation);
 		_textArea.setMinimumSize(new Dimension(0, 0));
 		_splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 		_splitPane.setResizeWeight(1);
-		_progressBar = progressBar;
 
 		// layout
 		add(_splitPane, BorderLayout.CENTER);
 		_splitPane.add(_textArea);
 		_bottomTabs = new JTabbedPane();
-		Supplier<String> titleSupplier = () -> _TAB_PREFIX + (char)('A' + level) + (++_searchId);
-		SearchPanelBottomArea tabContent = new SearchPanelBottomArea(this, progressBar, level);
-		_bottomTabs.addTab(titleSupplier.get(), tabContent);
-		_bottomTabs.setTabComponentAt(0, newTabHeader(titleSupplier.get(), tabContent));
-		_bottomTabs.addTab(_TAB_ADD, null);
+
+		// add first empty tab
+		SearchPanelBottomArea tabContent = new SearchPanelBottomArea(this, _progressBar, _level);
+		_bottomTabs.addTab(null, tabContent);
+		_bottomTabs.setTabComponentAt(0, newTabHeader(newTabTempTitle(), tabContent));
+
 		// add new tab code: I can't use a change listener to avoid loops
 		// the "+" tab should never be selected and must stick as last tab
-		Runnable addNewTab = () -> {
-			SearchPanelBottomArea body = new SearchPanelBottomArea(SearchPanel.this, progressBar, level);
-			body.setFont(getFont());
-			int idx = _bottomTabs.indexOfTab(_TAB_ADD);
-			_bottomTabs.insertTab(null, null, body, null, idx);
-			_bottomTabs.setTabComponentAt(idx, newTabHeader(titleSupplier.get(), body));
-			_bottomTabs.setSelectedIndex(idx);
-			invalidate();
-			if (_splitPane.getDividerLocation() >= _splitPane.getMaximumDividerLocation())
-				collapseBottomArea();
-		};
-		_bottomTabs.setTabComponentAt(1, UIUtils.newTabHeader(_TAB_ADD, null, addNewTab));
+		_bottomTabs.addTab(_TAB_ADD, null);
 		_bottomTabsNavigation = TabNavigation.createFor(_bottomTabs);
 		_splitPane.add(_bottomTabs);
 		// ugly but working (otherwise you have to hit "+" text)
@@ -79,9 +72,25 @@ public class SearchPanel extends JComponent implements FileDropListener {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				if (e.getButton() == 1)
-					addNewTab.run();
+					addNewTab();
 			}
 		});
+	}
+
+	private String newTabTempTitle() {
+		return _TAB_PREFIX + (char)('A' + _level) + (++_searchId);
+	}
+
+	private void addNewTab() {
+		SearchPanelBottomArea body = new SearchPanelBottomArea(SearchPanel.this, _progressBar, _level);
+		body.setFont(getFont());
+		int idx = _bottomTabs.indexOfTab(_TAB_ADD);
+		_bottomTabs.insertTab(null, null, body, null, idx);
+		_bottomTabs.setTabComponentAt(idx, newTabHeader(this.newTabTempTitle(), body));
+		_bottomTabs.setSelectedIndex(idx);
+		invalidate();
+		if (_splitPane.getDividerLocation() >= _splitPane.getMaximumDividerLocation())
+			collapseBottomArea();
 	}
 
 	private JComponent newTabHeader(String title, SearchPanelBottomArea tabContent) {
