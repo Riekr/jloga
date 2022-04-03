b6b42b6a95d5710fffaab40f6da49ec80a687552
Riekr <gparmiggiani@gmail.com>
2022-04-03 15:09:48 +0200

    HoverMonitor uses swing Timer instead of aux executor

--- src/main/java/org/riekr/jloga/ui/HoverMonitor.java
+++ src/main/java/org/riekr/jloga/ui/HoverMonitor.java
@@ -1,59 +1,43 @@
 package org.riekr.jloga.ui;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-
-import org.riekr.jloga.io.TextSource;
-import org.riekr.jloga.react.BoolConsumer;
-
+import javax.swing.*;
 import javax.swing.event.PopupMenuEvent;
 import javax.swing.event.PopupMenuListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import org.riekr.jloga.react.BoolConsumer;
 
 public class HoverMonitor extends MouseAdapter implements PopupMenuListener {
 
-	private final BoolConsumer _consumer;
-	private final long         _enterDelay, _exitDelay;
-
-	private Future<?> _mouseEnterdFuture, _mouseExitedFuture;
-	private boolean _mouseListenerEnabled = true;
+	private final Timer   _mouseEnter;
+	private final Timer   _mouseExit;
+	private       boolean _mouseListenerEnabled = true;
 
 	public HoverMonitor(BoolConsumer consumer) {
 		this(consumer, 200, 300);
 	}
 
-	public HoverMonitor(BoolConsumer consumer, long enterDelay, long exitDelay) {
-		_consumer = consumer;
-		_enterDelay = enterDelay;
-		_exitDelay = exitDelay;
+	public HoverMonitor(BoolConsumer consumer, int enterDelay, int exitDelay) {
+		_mouseEnter = new Timer(enterDelay, e -> consumer.accept(true));
+		_mouseEnter.setRepeats(false);
+		_mouseExit = new Timer(exitDelay, e -> consumer.accept(false));
+		_mouseExit.setRepeats(false);
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		if (_mouseListenerEnabled) {
-			if (_mouseExitedFuture == null || !_mouseExitedFuture.cancel(false)) {
-				if (_mouseEnterdFuture == null || _mouseEnterdFuture.isDone()) {
-					_mouseEnterdFuture = TextSource.MONITOR_EXECUTOR.schedule(() -> {
-						_mouseEnterdFuture = null;
-						_consumer.accept(true);
-					}, _enterDelay, TimeUnit.MILLISECONDS);
-				}
-			}
+			_mouseExit.stop();
+			_mouseEnter.start();
 		}
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		if (_mouseListenerEnabled) {
-			if (_mouseEnterdFuture == null || !_mouseEnterdFuture.cancel(false)) {
-				if (_mouseExitedFuture == null || _mouseExitedFuture.isDone()) {
-					_mouseExitedFuture = TextSource.MONITOR_EXECUTOR.schedule(() -> {
-						_mouseExitedFuture = null;
-						_consumer.accept(false);
-					}, _exitDelay, TimeUnit.MILLISECONDS);
-				}
-			}
+			_mouseEnter.stop();
+			_mouseExit.start();
 		}
 	}
 
